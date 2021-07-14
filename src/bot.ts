cc5ab4ba60fe895c4e26b632fdaf378051281e5d
rabwill <rabiawilliams@gmail.com>
2021-07-15 00:47:54 +1000

    Functionality overhaul

--- src/bot.ts
+++ src/bot.ts
@@ -1,11 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { ActivityHandler, MessageFactory, CardFactory, InvokeResponse, AdaptiveCardInvokeResponse, TurnContext, InvokeException } from 'botbuilder';
+import { ActivityHandler, MessageFactory, CardFactory, InvokeResponse, AdaptiveCardInvokeResponse, TurnContext, InvokeException, StatusCodes } from 'botbuilder';
 import * as ACData from "adaptivecards-templating";
 import * as cardOnefile from "./cards/cardOne.json";
 import * as cardTwofile from "./cards/cardTwo.json";
-import * as cardSenderFile from "./cards/cardSender.json";
 const CONVERSATION_DATA_PROPERTY = 'conversationData';
 export class EchoBot extends ActivityHandler {
     conversationData: any;
@@ -19,25 +18,24 @@ export class EchoBot extends ActivityHandler {
         this.conversationDataAccessor = conversationState.createProperty(CONVERSATION_DATA_PROPERTY);
 
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
-        this.onMessage(async (context, next) => {
-            // const replyText = `Echo: ${ context.activity.text }`;
-            // await context.sendActivity(MessageFactory.text(replyText, replyText));
+        this.onMessage(async (context, next) => {          
             //empty access for conversation data
             const conversationData = await this.conversationDataAccessor.get(context, {});
             //initial card or the base card   
-            var template = new ACData.Template(cardOnefile);
-            var userIds = [context.activity.from.id];
+            const template = new ACData.Template(cardOnefile);           
             //save the initiator id as the person who called the Bot          
             conversationData.initiator = context.activity.from.id;
-            //count of how many turns (to calculate the number of clicks the card got)
-            conversationData.turnCount = 0;
+            //count of how many clicks (to calculate the number of clicks the card got)
+            conversationData.clickCount = 0;
            //remove the bot at mentioned from the activity message
             const updatedText = TurnContext.removeRecipientMention(context.activity);
             conversationData.question=updatedText;
-            conversationData.voted = [context.activity.from.id];
+            conversationData.users = [context.activity.from.id];
+            conversationData.response=[];
             //set the conversation data   
             await this.conversationDataAccessor.set(context, conversationData);
-            var cardPayload = template.expand({ $root: { question:conversationData.question,userIds: userIds } });
+
+            const cardPayload = template.expand({ $root: { question:conversationData.question,userIds: [context.activity.from.id] } });
             const cardOne = CardFactory.adaptiveCard(cardPayload);
             cardOne.content.subtitle = "";
             const card = {
@@ -45,8 +43,7 @@ export class EchoBot extends ActivityHandler {
                 content: cardOne.content
                 
             };
-            await context.sendActivity({ attachments: [card] });
-            
+            await context.sendActivity({ attachments: [card] });            
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
@@ -62,49 +59,49 @@ export class EchoBot extends ActivityHandler {
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
-
-
-
     }
     //UAM onInvokeActivity overriden
-    async onInvokeActivity(context: TurnContext) {
+    async onInvokeActivity(context: TurnContext):Promise<InvokeResponse> {
         try {
-            const request = context.activity.value;
-            
-            if (request) {
-            const conversationData = await this.conversationDataAccessor.get(context); 
-            let turnCount = conversationData.turnCount || 0;             
-            var responseBody = {}
-            var payload={};            
-                switch (request.action.verb) {
-                    case 'vote': {
-                        //on button click , count the number of click and store it in conversation data
-                        conversationData.turnCount = ++turnCount;
-                        conversationData.voted.push(context.activity.from.id);
-                        await this.conversationDataAccessor.set(context, conversationData);
-                        payload = await this.processSend(conversationData);                       
-                        const cardOne = CardFactory.adaptiveCard(payload);
-                        const card = {
-                          contentType: cardOne.contentType,
-                          content: cardOne.content
-                      };
-                    const message =MessageFactory.attachment(card);                      
-                    message.id = context.activity.replyToId;
-                    await context.updateActivity(message);
-                    break;    
-                    }               
-                    case 'refresh': {
-                         payload = await this.processRefresh(context.activity.from.id,conversationData);
-                        break;                        
-                    }              
-                    
-                    default:
-                        throw new InvokeException(404);
+            let responseBody = {}
+            let payload={};   
+            if(context.activity.name==="adaptiveCard/action"){
+             
+                const request = context.activity.value;            
+                if (request) {
+                const conversationData = await this.conversationDataAccessor.get(context); 
+                let clickCount = conversationData.clickCount || 0; 
+                    switch (request.action.verb) {
+                        case 'ok': {
+                            //on button click , count the number of click and store it in conversation data
+                            conversationData.clickCount = ++clickCount;
+                            conversationData.users.push(context.activity.from.id);
+                            conversationData.response.push({name:context.activity.from.name,text:request.action.data.text});
+                            await this.conversationDataAccessor.set(context, conversationData);
+                            payload = await this.processSend(conversationData);                       
+                            const cardOne = CardFactory.adaptiveCard(payload);
+                            const card = {
+                              contentType: cardOne.contentType,
+                              content: cardOne.content
+                          };
+                        //message edits to refresh all users cards
+                        const message =MessageFactory.attachment(card);                      
+                        message.id = context.activity.replyToId;
+                        await context.updateActivity(message);
+                        break;    
+                        }               
+                        case 'refresh': {
+                             payload = await this.processRefresh(context.activity.from.id,conversationData);
+                            break;                        
+                        }      
+                        default:
+                         throw new InvokeException(StatusCodes.NOT_IMPLEMENTED);
+                    }
+                    responseBody= { statusCode: 200, type: "application/vnd.microsoft.card.adaptive", value: payload }
+                    return this.createInvokeResponse(responseBody);    
                 }
-                responseBody= { statusCode: 200, type: "application/vnd.microsoft.card.adaptive", value: payload }
-                return this.createInvokeResponse(responseBody);
-
             }
+           
 
         } catch (err) {
 
@@ -118,37 +115,68 @@ export class EchoBot extends ActivityHandler {
         return { status: 200, body };
     }
     //send the base card
-    async processSend(conversationData) {
-        var template = new ACData.Template(cardOnefile);        
-        var cardPayload = template.expand({ $root: { question:conversationData.question,userIds: conversationData.voted } });
-        return cardPayload
+    async processSend(conversationData:any) {
+        const template = new ACData.Template(cardOnefile);        
+        return template.expand({ $root: { question:conversationData.question,userIds: conversationData.users } });
     }
     //process refresh of cards based on the userIds array
-    async processRefresh(userId, conversationData) {
-      
-        var  template;  
-        //initiator
+    async processRefresh(userId:string, conversationData:any) {      
+        let  template;          
+        //initiator 
         if(conversationData.initiator===userId)   {
-         template = new ACData.Template(cardSenderFile);
-         return template.expand({ $root: {question:conversationData.question,turn: conversationData.turnCount, userIds:conversationData.voted } });
+         var payload=this.generateDynamicAdaptiveCard(conversationData.response)
+         template = new ACData.Template(payload);
+         return template.expand({ $root: {question:conversationData.question,click: conversationData.clickCount||0, userIds:conversationData.users } });
            
-        } //already voted  
-        else if (conversationData.voted.indexOf(userId)>-1&&conversationData.initiator!==userId){
-             template = new ACData.Template(cardTwofile);
-             return template.expand({ $root: { question:conversationData.question, userIds: conversationData.voted } });
-          
-         }   
-       
+        } //already responded  
+        else if (conversationData.users.indexOf(userId)>-1&&conversationData.initiator!==userId){
+            template = new ACData.Template(cardTwofile);
+            return template.expand({ $root: { question:conversationData.question, userIds: conversationData.users } });
+         }          
     }
-    /**
-   * Override the ActivityHandler.run() method to save state changes after the bot logic completes.
-   */
+    
     async run(context) {
         await super.run(context);
-
         // Save any state changes. The load happened during the execution of the Dialog.
         await this.conversationState.saveChanges(context, false);
 
+    }
+    //generate card with responses from mates
+    generateDynamicAdaptiveCard(response) {
+       
+        var body = [  {
+            "type": "TextBlock",
+            "text": "Hi initiator, ${click} team mates responded to your question : ' ${question} ' 💥",
+            "wrap": true,
+            "color": "Accent"
+          }  ];
+            response.forEach(item => {
+                body.push({
+                    "type": "TextBlock",
+                    "text":item.name+" says '"+item.text+"'",
+                    "wrap": true,
+                    "color": "good"
+                }  );
+            });            
+    
+        var card = {
+            "type": "AdaptiveCard",   
+            body,
+            "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+            "version": "1.4",
+            "refresh": {
+              "userIds": "${userIds}",
+              "action": {
+                "type": "Action.Execute",
+                "verb": "refresh",
+                "title": "Refresh",
+                "data": {
+                  "cardId": "${cardId}"
+                }
+              }
+            }
+          }   
+        return card;
     }
 }
 
